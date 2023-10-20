7a80b21996df8d622e5c94cd848dc0efe3855382
JEONGHWANMIN <ghksals09041@gmail.com>
2023-10-20 21:34:22 +0900

    Style : fonts 파일 분리

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -4,27 +4,6 @@ import reset from "styled-reset";
 const GlobalStyles = createGlobalStyle`
     ${reset}
 
-    @font-face {
-    font-family: 'Pretendard-SemiBold';
-    src: url('../../public/fonts/Pretendard-SemiBold.subset.woff2') format('woff');
-    font-weight: 600;
-    font-display: swap;
-    }
-
-    @font-face {
-    font-family: 'Pretendard-Medium';
-    src: url('../../public/fonts/Pretendard-Medium.subset.woff2') format('woff');
-    font-weight: 500;
-    font-display: swap;
-    }
-
-    @font-face {
-    font-family: 'Pretendard-Regular';
-    src: url('../../public/fonts/Pretendard-Regular.subset.woff2') format('woff');
-    font-weight: 400;
-    font-display: swap;
-    }
-
     *{
         box-sizing: border-box;
         font-size: 62.5;
