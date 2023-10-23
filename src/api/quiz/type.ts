ddf5c2363a7acd56ea4e6a3dfcfbaf51ac71c87f
JEONGHWANMIN <mimi0j0j@naver.com>
2023-10-23 17:20:39 +0900

    Feat : 타입 세분화

--- src/api/quiz/type.ts
+++ src/api/quiz/type.ts
@@ -1,7 +1,10 @@
+export type QUIZ_TYPE = "multiple" | "boolean";
+export type QUIZ_DIFFICULTY = "easy" | "medium" | "hard";
+
 export interface QuizQuestion {
   category: string;
-  type: "multiple" | "boolean";
-  difficulty: "easy" | "medium" | "hard";
+  type: QUIZ_TYPE;
+  difficulty: QUIZ_DIFFICULTY;
   question: string;
   correct_answer: string;
   incorrect_answers: string[];
