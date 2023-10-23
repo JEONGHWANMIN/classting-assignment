f3776d5c3d0f075bd7cd42f14753879baeed34da
JEONGHWANMIN <mimi0j0j@naver.com>
2023-10-23 18:22:57 +0900

    Refactor : 퀴즈 보기 객체 키 값 변경

--- src/pages/quiz/setting/_utils/convertQuizListToQuizDetailList.ts
+++ src/pages/quiz/setting/_utils/convertQuizListToQuizDetailList.ts
@@ -5,13 +5,13 @@ import { shuffleArray } from "@src/utils/shuffleArray";
 export const convertQuizListToQuizDetailList = (quizList: QuizQuestion[]) => {
   const quizDetailList = quizList.map((quiz) => {
     const answers = [quiz.correct_answer, ...quiz.incorrect_answers];
-    const shuffledOptions = shuffleArray(answers);
+    const shuffledAnswers = shuffleArray(answers);
     const decodedQuestion = decodeHtmlEntities(quiz.question);
 
     return {
       ...quiz,
       question: decodedQuestion,
-      shuffledOptions,
+      shuffledAnswers,
       isAnswered: false,
     };
   });
