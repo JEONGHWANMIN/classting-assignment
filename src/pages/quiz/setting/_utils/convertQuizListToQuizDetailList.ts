ae92902cfbea3c88ec27336cbbba0e521b871ab5
JEONGHWANMIN <mimi0j0j@naver.com>
2023-10-23 17:22:13 +0900

    Feat : 퀴즈 질문에 HTML 기호 변환 추가

--- src/pages/quiz/setting/_utils/convertQuizListToQuizDetailList.ts
+++ src/pages/quiz/setting/_utils/convertQuizListToQuizDetailList.ts
@@ -1,14 +1,17 @@
 import { QuizQuestion } from "@src/api/quiz/type";
+import { decodeHtmlEntities } from "@src/utils/decodeHtmlEntities";
 import { shuffleArray } from "@src/utils/shuffleArray";
 
 export const convertQuizListToQuizDetailList = (quizList: QuizQuestion[]) => {
   const quizDetailList = quizList.map((quiz) => {
     const answers = [quiz.correct_answer, ...quiz.incorrect_answers];
-    const shuffledQuizList = shuffleArray(answers);
+    const shuffledOptions = shuffleArray(answers);
+    const decodedQuestion = decodeHtmlEntities(quiz.question);
 
     return {
       ...quiz,
-      shuffledQuizList,
+      question: decodedQuestion,
+      shuffledOptions,
       isAnswered: false,
     };
   });
