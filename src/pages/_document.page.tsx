fe52ab222592c0dd514282e2242a81458e2ffc87
JEONGHWANMIN <mimi0j0j@naver.com>
2023-10-25 03:16:37 +0900

    Feat : 서버 스타일 적용 추가

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,50 +1,51 @@
 import React from "react";
 import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 import type { DocumentContext } from "next/document";
-import { ServerStyleSheet } from "styled-components"; // styled-components 추가
 
-const MyDocument = () => (
-  <Html lang="en">
-    <Head />
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const cache = createCache();
+    const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet();
 
-MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const cache = createCache();
-  const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <StyleProvider cache={cache}>
+              {sheet.collectStyles(<App {...props} />)}
+            </StyleProvider>
+          ),
+      });
 
-  const sheet = new ServerStyleSheet();
+    const initialProps = await Document.getInitialProps(ctx);
+    const style = extractStyle(cache, true);
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) =>
-        (
-          <StyleProvider cache={cache}>
-            <App {...props} />
-          </StyleProvider>
-        ),
-    });
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style dangerouslySetInnerHTML={{ __html: style }} />
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  }
 
-  const initialProps = await Document.getInitialProps(ctx);
-  const style = extractStyle(cache, true);
-
-  const styles = sheet.getStyleElement();
-
-  return {
-    ...initialProps,
-    styles: (
-      <>
-        {initialProps.styles}
-        <style dangerouslySetInnerHTML={{ __html: style }} />
-        {styles}
-      </>
-    ),
-  };
-};
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
 export default MyDocument;
